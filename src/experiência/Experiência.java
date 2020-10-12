06f8cc39156f49a2a498e2ab9b9f47a44db15a6b
luisf <luisf@LAPTOP-1IKSVDUE.lan>
2020-10-12 11:34:55 +0100

    primeiro commit

---
+++ src/experiência/Experiência.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package experiência;
+
+/**
+ *
+ * @author luisf
+ */
+public class Experiência {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("cona boa");
+        // TODO code application logic here
+    }
+    
+}
